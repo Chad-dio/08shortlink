7d11adad1d8e8107c0a5ae8e00d8115a48c83310
Chad <2420811595@qq.com>
2024-08-04 00:13:49 +0800

    feature:初始化短链接服务模块

--- project/src/main/java/org/chad/shortlink/project/ShortlinkProjectApplication.java
+++ project/src/main/java/org/chad/shortlink/project/ShortlinkProjectApplication.java
@@ -1,9 +1,11 @@
 package org.chad.shortlink.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.chad.shortlink.project.mapper")
 public class ShortlinkProjectApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShortlinkProjectApplication.class, args);
